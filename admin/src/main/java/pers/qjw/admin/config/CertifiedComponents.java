393725f2efb6b03ccbb02702bb7ed9848b7a32ad
qiujiawe <1967842929@qq.com>
2021-04-14 10:42:49 +0800

    添加了 记录文章被浏览次数 的功能

--- admin/src/main/java/pers/qjw/admin/config/CertifiedComponents.java
+++ admin/src/main/java/pers/qjw/admin/config/CertifiedComponents.java
@@ -1,27 +1,31 @@
 package pers.qjw.admin.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import pers.qjw.admin.interceptor.AccessRecordInterceptor;
 import pers.qjw.admin.interceptor.TokenInterceptor;
+import pers.qjw.admin.mapper.BlogMapper;
 import pers.qjw.admin.resolver.CurrentUserResolver;
 
 import java.util.List;
 
 @Configuration
+@AllArgsConstructor
 public class CertifiedComponents implements WebMvcConfigurer {
 
-    /**
-     * 该方法用于添加拦截器
-     * @param registry springframework提供的对象。用于添加拦截器，配置拦截器的功能
-     */
+    private final BlogMapper blogMapper;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 刨除掉 swagger2 相关的url，不然 swagger2 无法正常生成帮助文档
         registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**")
                 .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+        registry.addInterceptor(new AccessRecordInterceptor(blogMapper)).addPathPatterns("/api/articles/**");
     }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new CurrentUserResolver());
---
+++ admin/src/main/java/pers/qjw/admin/interceptor/AccessRecordInterceptor.java
@@ -0,0 +1,62 @@
+package pers.qjw.admin.interceptor;
+
+import com.google.common.base.Strings;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import pers.qjw.admin.config.Constants;
+import pers.qjw.admin.exception.UserIllegalArgumentException;
+import pers.qjw.admin.mapper.BlogDynamicSqlSupport;
+import pers.qjw.admin.mapper.BlogMapper;
+import pers.qjw.admin.model.Blog;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.mybatis.dynamic.sql.SqlBuilder.isEqualTo;
+
+@AllArgsConstructor
+public class AccessRecordInterceptor implements HandlerInterceptor {
+
+    private final BlogMapper blogMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        String method = request.getMethod();
+        // 筛掉get意外的方法
+        if (Objects.equals(method, "GET")) {
+            // 没有登录的游客每请求一次文章就算浏览了一次文章
+            String token = request.getHeader(Constants.AUTHORIZATION);
+            if (Strings.isNullOrEmpty(token) || "null".equals(token)) {
+                // 从uri里获取文章id
+                String[] uri = request.getRequestURI().split("/");
+                int id;
+                try {
+                    id = Integer.parseInt(uri[uri.length - 1]);
+                } catch (Exception e) {
+                    throw new UserIllegalArgumentException(HttpStatus.BAD_REQUEST, "未找到指定资源");
+                }
+
+                Optional<Blog> optional = blogMapper.selectOne(c -> c.where(BlogDynamicSqlSupport.id, isEqualTo(id)));
+                if (optional.isPresent()) {
+                    Blog blog = optional.get();
+                    // 更新数据
+                    blogMapper.update(c -> c.set(BlogDynamicSqlSupport.views).equalTo(blog.getViews() + 1)
+                            .where(BlogDynamicSqlSupport.id, isEqualTo(id)));
+                } else {
+                    throw new UserIllegalArgumentException(HttpStatus.BAD_REQUEST, "未找到指定资源");
+                }
+            }
+        }
+        return true;
+    }
+
+}
