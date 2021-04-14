2f521a64797e31418ff25fd7d0cfdbfe65d2b24b
qiujiawe <1967842929@qq.com>
2021-04-14 12:42:01 +0800

    修复了一些bug

--- admin/src/main/java/pers/qjw/admin/controller/ColumnController.java
+++ admin/src/main/java/pers/qjw/admin/controller/ColumnController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pers.qjw.admin.annotations.Authorization;
 import pers.qjw.admin.model.BlogColumn;
 import pers.qjw.admin.service.ColumnService;
 
